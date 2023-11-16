0acf5866bb46409d747cb61488bf1c608d8e23d1
yuu3415 <buddyyou6@gmail.com>
2023-11-16 16:34:30 +0900

    sqlファイルにユニーク制約を追加したが同じタイトルのデータが作れてしまう。一旦PR

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -5,9 +5,11 @@ CREATE TABLE music (
   title VARCHAR(20) NOT NULL,
   singer VARCHAR(20) NOT NULL,
   PRIMARY KEY(id)
+  UNIQUE KEY(title)
 );
 
-INSERT INTO music (id,title, singer) VALUES (1,"The Beginning", "OneOkRock");
-INSERT INTO music (id,title, singer) VALUES (2,"Pretender", "Official髭男dism");
-INSERT INTO music (id,title, singer) VALUES (3,"Lemon", "米津玄師");
-INSERT INTO music (id,title, singer) VALUES (4,"打上花火", "DAOKO×米津玄師");
+
+INSERT INTO music (id, title, singer) VALUES (1, 'The Beginning', 'OneOkRock');
+INSERT INTO music (id, title, singer) VALUES (2, 'Pretender', 'Official髭男dism');
+INSERT INTO music (id, title, singer) VALUES (3, 'Lemon', '米津玄師');
+INSERT INTO music (id, title, singer) VALUES (4, '打上花火', 'DAOKO×米津玄師');
--- src/main/java/com/example/homework10/entity/Music.java
+++ src/main/java/com/example/homework10/entity/Music.java
@@ -4,7 +4,6 @@ import java.util.Objects;
 
 public class Music {
     private int id;
-
     private String title;
 
     private String singer;
