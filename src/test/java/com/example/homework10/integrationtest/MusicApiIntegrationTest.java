e7c3ade776a2f50060cad23ddaa993804c034bda
yuu3415 <buddyyou6@gmail.com>
2023-11-23 10:17:39 +0900

    結合テストを実装

---
+++ src/test/java/com/example/homework10/integrationtest/MusicApiIntegrationTest.java
@@ -0,0 +1,83 @@
+package com.example.homework10.integrationtest;
+
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.spring.api.DBRider;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.charset.StandardCharsets;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DBRider
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class MusicApiIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @Transactional
+    @DataSet("datasets/music.yml")
+    void ミュージックが全て取得できること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/music"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("""
+                [
+                    {
+                        "id":1,
+                        "title":"The Beginning",
+                        "singer":"OneOkRock"
+                    },
+                    {
+                        "id":2,
+                        "title":"Pretender",
+                        "singer":"Official髭弾dism"
+                    },
+                    {
+                        "id":3,
+                        "title":"Lemon",
+                        "singer":"米津玄師"
+                    }
+                ]
+                """, response, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DataSet(value = "datasets/music.yml")
+    @Transactional
+    void 指定したIDのミュージックが取得できること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/music/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("""
+                {
+                    "id":1,
+                    "title":"The Beginning",
+                    "singer":"OneOkRock"
+                }
+                """, response, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DataSet(value = "datasets/music.yml")
+    @Transactional
+    void 指定したIDが存在しない場合に404が返ること() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/music/100"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn().getResponse().getErrorMessage();
+    }
+
+}
